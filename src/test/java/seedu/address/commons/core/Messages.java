9503b02869a726dcde75a5c6ce9e48394387f336
JustWeiHao <e0960531@u.nus.edu>
2024-03-01 19:37:07 +0800

    Add remark command

---
+++ src/test/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.core;
+
+/**
+ * Container for user visible messages.
+ */
+public class Messages {
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+}
